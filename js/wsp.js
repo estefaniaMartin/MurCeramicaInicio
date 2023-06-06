71154fc36b2f1ae289aa5c5520c3e83c487ee76b
Estefania Martin <119458464+estefaniaMartin@users.noreply.github.com>
2023-06-06 19:56:57 -0300

    se quitó el número de wsp por seguridad.js

--- js/wsp.js
+++ js/wsp.js
@@ -1,6 +1,6 @@
 let btnWasp = `
 <div class="whatsapp">
-    <a class="btn-wsp" href="https://web.whatsapp.com/send?phone=541158755260&amp;text=Realizar%20una%20consulta."
+    <a class="btn-wsp" href="https://web.whatsapp.com/send?phone=#&amp;text=Realizar%20una%20consulta."
     rel="nofollow" target="_blank"><img src="./img/wsp.png" class="whatsapp-icon" alt="wsp">
     </a>
 </div>
